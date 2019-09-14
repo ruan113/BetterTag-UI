919c4c673ae5d46d01870c1648254b22031bb16d
Ruan Michel Adabo <ruanbento@gmail.com>
2019-09-14 19:03:44 -0300

    [UPDATE] Criação inicial dos componentes e ajustes iniciais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ImageViewerComponent } from './pages/image-viewer/image-viewer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ImageViewerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { ImageViewerComponent } from './pages/image-viewer/image-viewer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageViewerComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
