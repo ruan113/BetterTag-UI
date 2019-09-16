4f950560f6387a1858b450b138008bee7289af7e
Ruan Michel Adabo <ruanbento@gmail.com>
2019-09-16 02:58:42 -0300

    [UPDATE] Implementacao API para Photos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ImageViewerComponent } from './pages/image-viewer/image-viewer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PhotosService } from './services/photos/photos.service';
+import { ApiService } from './services/api/api.service';
 
 @NgModule({
   declarations: [
@@ -17,8 +20,12 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    PhotosService,
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
