70b36a89d274b7f70cc8a5f00b5380b15e1f6873
Ruan Michel Adabo <ruanbento@gmail.com>
2019-09-24 21:14:03 -0300

    [CREATE] Criação da statusBoard e ajustes na modularização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ImageViewerComponent } from './pages/image-viewer/image-viewer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { PhotosService } from './services/photos/photos.service';
 import { ApiService } from './services/api/api.service';
 import { SafePipe } from './pipes/safe-pipe';
-import { ContentFilterComponent } from './components/content-filter/content-filter.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImageViewerComponent,
-    HeaderComponent,
-    FooterComponent,
     SafePipe,
-    ContentFilterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    ComponentsModule
   ],
   providers: [
     PhotosService,
